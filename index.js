8f62a8364e4352d1047a8d00a616d2b3e1c54752
Milena <milenagg77@hotmail.com>
2023-01-09 22:56:41 +0000

    code to track what the total change in profits are from month to month.

--- index.js
+++ index.js
@@ -101,21 +101,21 @@ for (var i = 0; i < totalMonths; i++) {
 }
 console.log(netProfitLosses);
 
-// The average of the changes in Profit/Losses over the entire period.
+// // The average of the changes in Profit/Losses over the entire period.
 
-// You will need to track what the total change in profits are from month to month and then find the average.
-// (Total/Number of months)
+// Code track what the total change in profits are from month to month.
+// // (Total/Number of months)
 
-let totalChange = 0;
+// let totalChange = 0;
 
-for (var i = 1; i < totalMonths; i++ ) {
-   totalChange += finances[i][1] - finances[i-1][1];
-}
-console.log(totalChange);
+// for (var i = 1; i < totalMonths; i++ ) {
+//    totalChange += finances[i][1] - finances[i-1][1];
+// }
+// console.log(totalChange);
 
-let averageChanges = totalChange / 85;
+// let averageChanges = totalChange / 85;
 
-console.log(averageChanges)
+// console.log(averageChanges)
 
 // The greatest increase in profits (date and amount) over the entire period.
 
